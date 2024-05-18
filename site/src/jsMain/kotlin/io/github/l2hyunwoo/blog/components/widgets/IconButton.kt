476174cbf23d573b8e46dd3cad72854eb49b9215
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-05-18 10:39:44 +0900

    :tada: INITIAL COMMIT

---
+++ site/src/jsMain/kotlin/io/github/l2hyunwoo/blog/components/widgets/IconButton.kt
@@ -0,0 +1,21 @@
+package io.github.l2hyunwoo.blog.components.widgets
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.setVariable
+import com.varabyte.kobweb.silk.components.forms.Button
+import com.varabyte.kobweb.silk.components.forms.ButtonVars
+import org.jetbrains.compose.web.css.em
+import io.github.l2hyunwoo.blog.CircleButtonVariant
+import io.github.l2hyunwoo.blog.UncoloredButtonVariant
+
+@Composable
+fun IconButton(onClick: () -> Unit, content: @Composable () -> Unit) {
+    Button(
+        onClick = { onClick() },
+        Modifier.setVariable(ButtonVars.FontSize, 1.em), // Make button icon size relative to parent container font size
+        variant = CircleButtonVariant.then(UncoloredButtonVariant)
+    ) {
+        content()
+    }
+}
